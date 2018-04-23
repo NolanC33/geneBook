ff2867f0e7ebe4e5b0e18125463d1f077d2567e4
Nolan Crane <nolancrane@yahoo.com>
2018-04-23 18:29:16 -0400

    Adding in preamble and postamble files, adding escape_bad_chars method

--- removeUnprintable.py
+++ removeUnprintable.py
@@ -3,7 +3,7 @@ import string
 
 class MakePretty:
     oldVersion = open("./v1.txt", 'r')
-    newVersion = open("./v2.txt", 'w+', encoding='utf-8')
+    newVersion = open("./v2.tex", 'w+', encoding='utf-8')
     printable = set(string.printable)
 
     def __init__(self):
@@ -16,21 +16,52 @@ class MakePretty:
         return line
 
     def clean_and_parse_line(self, line):
-        cleaned =  ''.join(filter(lambda x: x in self.printable, line))
+        cleaned = ''.join(filter(lambda x: x in self.printable, line))
 
         if MakePretty.check_if_removable(cleaned):
             return ''
 
-        return cleaned
+        return MakePretty.escape_bad_chars(cleaned)
+
+    @staticmethod
+    def escape_bad_chars(line):
+
+        new_line = line
+
+        things_to_escape = ["\\", "&", "%", "$", "#", "_", "^"]
+
+        for rep in things_to_escape:
+            new_line = new_line.replace(rep, "\\" + rep)
+
+        return new_line
 
     @staticmethod
     def check_if_removable(line):
-        split_version = line.split(" ")
+        split_version = line.strip().split(" ")
 
-        if len(split_version) == 3 and "CRANE GENEALOGY" in line:
+        if len(split_version) == 3 and "CRANE GENEALOGY." in line:
             return True
 
         return False
+
+    def generate(self):
+        self.oldVersion = open("./v1.txt", 'r')
+        self.newVersion = open("./v2.tex", 'w+', encoding='utf-8')
+
+        preamble = open("./preamble.txt", "r")
+        postamble = open("./postamble.txt", "r")
+
+        self.newVersion.write(preamble.read())
+
+        self.convert()
+
+        self.newVersion.write(postamble.read())
+
+        self.oldVersion.close()
+        self.newVersion.close()
+
+        preamble.close()
+        postamble.close()
 
     def convert(self):
 
@@ -42,8 +73,6 @@ class MakePretty:
                 self.newVersion.write(fixed_line)
             this_line = self.oldVersion.readline()
 
-        self.oldVersion.close()
-        self.newVersion.close()
 
 
-MakePretty().convert()
+MakePretty().generate()
